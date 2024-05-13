77e64af0b90bbf85fe234beaf509260480c0c290
LanQian <5499636+LanQian528@users.noreply.github.com>
2024-05-14 05:32:47 +0800

    fix wss bugs

--- chatgpt/wssClient.py
+++ chatgpt/wssClient.py
@@ -8,8 +8,7 @@ wss_map = {}
 async def ac2wss(access_token):
     if access_token in wss_map:
         if int(time.time()) - wss_map.get(access_token, {}).get("timestamp", 0) < 60 * 60:
-            wss_map[access_token]["timestamp"] = int(time.time())
-            wss_url = wss_map[access_token] = wss_map[access_token]["wss_url"]
+            wss_url = wss_map[access_token]["wss_url"]
             logger.info(f"access_token -> wss_url from cache")
             return True, wss_url
         else:
