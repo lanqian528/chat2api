c0165755e23a3a949354365c26b56e55f26be3e3
LanQian <5499636+LanQian528@users.noreply.github.com>
2024-06-22 20:16:08 +0800

    v1.3.4 show req_token

--- chatgpt/ChatService.py
+++ chatgpt/ChatService.py
@@ -31,6 +31,7 @@ class ChatService:
 
     async def set_dynamic_data(self, data):
         if self.req_token:
+            logger.info(f"Request token: {self.req_token}")
             req_len = len(self.req_token.split(","))
             if req_len == 1:
                 self.access_token = await verify_token(self.req_token)
@@ -39,6 +40,7 @@ class ChatService:
                 self.access_token = await verify_token(self.req_token.split(",")[0])
                 self.account_id = self.req_token.split(",")[1]
         else:
+            logger.info("Request token is empty, use no-auth 3.5")
             self.access_token = None
             self.account_id = None
 
