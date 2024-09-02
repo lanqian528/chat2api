fdf869414f4f667d7eb3edbe23e60c1a41586c1c
najxjdn@gmail.com <najxjdn@gmail.com>
2024-09-02 03:03:10 +0000

    Automatically migrate old app features

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#
+# This repository is an Anvil app. Learn more at https://anvil.works/
+# To run the server-side code on your own machine, run:
+# pip install anvil-uplink
+# python -m anvil.run_app_via_uplink YourAppPackageName
+
+__path__ = [__path__[0]+"/server_code", __path__[0]+"/client_code"]
